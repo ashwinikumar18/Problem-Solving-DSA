55252ecdedec10ec8274db57a47bdfd0946a6a88
Ashwini Kumar <91528679+ashwinikumar18@users.noreply.github.com>
2023-06-25 13:57:36 +0530

    5. Print 1 to 5
    5. Reverse print

---
+++ Reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Reverse {
+    public static void printNum(int n){
+        if( n==6){
+            return;
+        }
+        System.out.println(n);
+        printNum(n+1);
+    }
+    public static void main(String args[]){
+        int n=0;
+        printNum(n+1);
+    }
+}
