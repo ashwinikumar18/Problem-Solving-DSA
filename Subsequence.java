64daf875f2035c5543e57fe0b004243a8a545e29
Ashwini Kumar <91528679+ashwinikumar18@users.noreply.github.com>
2023-07-03 22:37:57 +0530

    All outcomes of the string
    16. Subsequence of strings

---
+++ Subsequence.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Subsequence {
+    public static void subsequence(String str, int idx, String newString){
+        if(idx == str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+
+        subsequence(str, idx+1, newString+currChar);
+
+        subsequence(str, idx+1, newString);
+    }
+    public static void main(String args[]){
+    String str = "abc";
+    subsequence(str, 0, "");
+    }
+}
