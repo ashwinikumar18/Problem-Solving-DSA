03232d16a3fdcd8b84b43f4d6aa2715bb7bd05bd
Ashwini Kumar <91528679+ashwinikumar18@users.noreply.github.com>
2023-07-06 22:56:15 +0530

    Finding final result using Backtracking
    19. Backtracking

---
+++ Backtracking.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Backtracking {
+    public static void printPermutation(String str, String permu, int idx){
+        if(str.length() == 0){
+            System.out.println(permu);
+            return;
+        }
+        for(int i=0; i<str.length(); i++){
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i+1);
+            printPermutation(newStr, permu + currChar, idx+1);
+        }
+    }
+    public static void main(String args[]){
+        String str= "ABC";
+        printPermutation(str, "", 0);
+    }
+}
